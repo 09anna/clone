ff288491735454e3fc381efc4be0d60bc2e21868
anna <annayunarai09@gmail.com>
2025-06-14 21:44:39 +0545

    Add comment,conversation,message,post,user models

---
+++ Backend/models/comment.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    test: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true
+    }
+});
+export default commentSchema = mongoose.model('Comment', commentSchema);
