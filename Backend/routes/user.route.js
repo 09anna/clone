7bb596a8b6f376c7449da3181b42a18bf4e8be98
anna <annayunarai09@gmail.com>
2025-06-21 13:02:12 +0545

    Add user authentication routes: register, login, logout and profile view

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,5 +1,12 @@
 import express from 'express';
-import { register } from '../controllers/user.controller.js';
+import { getProfile, login, logout, register } from '../controllers/user.controller.js';
+import isAuthenticated from '../middlewares/isAuthenticated.js';
 const router =express.Router();
 
 router.route('/register').post(register);
+router.route('/login').post(login);
+router.route('/logout').get(logout);
+router.route('/:id/profile').get(isAuthenticated, getProfile);
+
+
+
