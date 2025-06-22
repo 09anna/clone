184dc2aa645aadf90b970893a7b63116707e5273
anna <annayunarai09@gmail.com>
2025-06-22 14:57:06 +0545

    add likePost logic

--- Backend/controllers/post.controller.js
+++ Backend/controllers/post.controller.js
@@ -27,15 +27,15 @@ export const addNewPost = async (req, res) => {
             author: authorId
         });
 
-        const user= await User.findById(authorId);
-        if(user){
+        const user = await User.findById(authorId);
+        if (user) {
             user.posts.push(post._id);
             await user.save();
         }
 
-        await post.populate({path:'author', select:'-password'});
+        await post.populate({ path: 'author', select: '-password' });
         return res.status(201).json({
-            message:"New post added",
+            message: "New post added",
             post,
             success: true
         })
@@ -43,4 +43,85 @@ export const addNewPost = async (req, res) => {
         console.log(error);
     }
 }
+
+export const getAllPost = async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ createdAt: -1 })
+            .populate({ path: 'author', select: 'username, profilePicture' })
+            .populate({
+                path: 'comments', sort: { createdAt: -1 }, populate: {
+                    path: 'author',
+                    select: 'username, profilePicture'
+                }
+            });
+        return res.status(200).json({
+            posts,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getUserPost = async (req, res) => {
+    try {
+        const authorId = req.id;
+        const posts = await Post.find({ author: authorId }).sort({ createdAt: -1 })
+            .populate({
+                path: 'author',
+                select: 'username, profilePicture'
+            }).populate({
+                path: 'comments',
+                sort: { createdAt: -1 },
+                populate: {
+                    path: 'author',
+                    select: 'username, profilePicture'
+                }
+            });
+        return res.status(200).json({
+            posts,
+            success: true
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+//Like post
+export const likePost = async (req, res) => {
+    try {
+        const likeKrneWalaUserKiId = req.id;
+        const postId = req.params.id;
+        const post = await Post.findById(postId);
+        if (!post) {
+            return res.status(404).json({
+                message: 'Post Not Found!',
+                success: false
+            })
+
+        }
+        //like logic starts
+        await post.updateOne({ $addToSet: { likes: likeKrneWalaUserKiId } });
+
+        await post.save();
+
+        //implementing socket io for real-time notifications
+
+
+
+
+        return res.status(200).json({
+            message:"post liked",
+            success:true
+        });
+
+
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+
+
 
