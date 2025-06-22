ad7b52f81e7826b3294061a8af136e33496ff31a
anna <annayunarai09@gmail.com>
2025-06-22 22:22:27 +0545

    Establish conversation logic

--- Backend/controllers/message.controller.js
+++ Backend/controllers/message.controller.js
@@ -1,4 +1,4 @@
-import {Conversation} from "../models/conversation.model.js";
+import { Conversation } from "../models/conversation.model.js";
 import { Message } from "../models/message.model.js";
 //for messaging
 export const sendMessage=async(req, res)=>{
@@ -7,21 +7,61 @@ export const sendMessage=async(req, res)=>{
         const receiverId=req.params.id;
         const {message}=req.body;
 
-        let converstion=await Conversation.findOne({
+        let conversation=await Conversation.findOne({
             participants:{$all:[senderId, receiverId]}
         });
 
         //establish the conversation
-        if(!converstion){
-            converstion=await Conversation.create({
+        if(!conversation){
+            conversation=await Conversation.create({
                 participants:[senderId, receiverId]
             })
         }
-        const newMessage=await Message
+        const newMessage=await Message.create({
+            senderId,
+            receiverId,
+            message
+        });
+
+        if(newMessage) Conversation.messages.push(newMessage._id);
+        await Promise.all([conversation.save(), newMessage.save()]);
+
+        //implement socket for real time data transfer
 
 
+
+
+        return res.status(201).json({
+            success: true,
+            newMessage
+        });
     } catch (error) {
         console.log(error);
         
+    }
+}
+
+export const getMessage=async(req, res)=>{
+    try {
+        const senderId=req.id;
+        const receiverId=req.params.id;
+
+        const conversation = await Conversation.find({
+            participants: {$all : [senderId, receiverId] }
+        });
+        
+        if(!conversation){
+            return res.status(200).json({
+                success: true,
+                messages:[]
+            });
+        }
+
+        return res.status(200).json({
+            success:true,
+            messages:conversation?.messages
+        });
+    } catch (error) {
+        console.log(error);
     }
 }
