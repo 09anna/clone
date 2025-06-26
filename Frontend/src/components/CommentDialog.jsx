e94effbeac1d901422f89a2f7fcf5a037e442357
anna <annayunarai09@gmail.com>
2025-06-26 14:14:39 +0545

    Initial commit

---
+++ Frontend/src/components/CommentDialog.jsx
@@ -0,0 +1,13 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Dialog, DialogContent } from "@radix-ui/react-dialog";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const CommentDialog = ({ open, setOpen }) => {
+
+    return (
+        <div></div>
+    )
+}
+
+export default CommentDialog;
