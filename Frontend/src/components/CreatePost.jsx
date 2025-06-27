94744a39ee683628c332ab5f7df4544110c70073
anna <annayunarai09@gmail.com>
2025-06-27 22:42:49 +0545

    Add dialog components, avatar components and post handler function

--- Frontend/src/components/CreatePost.jsx
+++ Frontend/src/components/CreatePost.jsx
@@ -1,7 +1,12 @@
 import { Dialog, DialogContent } from "@radix-ui/react-dialog";
 import React from "react";
+import { DialogHeader } from "./ui/dialog";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Textarea } from "@/components/ui/textarea.jsx";
 
-const CreatePost=()=>{
+
+const CreatePost=({open, setOpen})=>{
+    
     const createPostHandler=async(e)=>{
         e.preventDefault();
         try {
@@ -14,9 +19,21 @@ const CreatePost=()=>{
     return (
         <Dialog open={open}>
             <DialogContent onInteractOutside={()=>setOpen(false)}>
-                <form onSubmit={createPostHandler}>
+                <DialogHeader className="font-semibold text-center ">Create New Post</DialogHeader>
+                <div className="flex gap-3 items-center">
+                    <Avatar>
+                        <AvatarImage src="" alt="Image"/>
+                            <AvatarFallback>CN</AvatarFallback>                        
+                    </Avatar>
+                    <div>
+                        <h1 className='font-semibold text-xs'>Username</h1>
+                        <span className='text-gray-600 text-xs'>Bio here...</span>
+                    </div>
+                </div>
+                <Textarea className="focus-visible:ring-transparent border-none" placeholder="Write a caption..."/>
 
-                </form>
+                
+                
 
             </DialogContent>
         </Dialog>
