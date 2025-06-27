452601e7ca8320339c15fc8d0be7c5c158ac3212
anna <annayunarai09@gmail.com>
2025-06-27 09:12:15 +0545

    Add Provider wrapper component from react-redux

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Toaster } from './components/ui/sonner'
+import { Provider } from 'react-redux'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-    <Toaster/>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider>
+      <App />
+      <Toaster />
+    </Provider>
+  </React.StrictMode>,
 )
