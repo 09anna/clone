91135e1f34f50f529f2effc08ce15050cb060058
anna <annayunarai09@gmail.com>
2025-06-27 22:45:52 +0545

    import persist store and persistgate

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,12 +4,19 @@ import './index.css'
 import App from './App.jsx'
 import { Toaster } from './components/ui/sonner'
 import { Provider } from 'react-redux'
+import store from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import persistStore from 'redux-persist/es/persistStore'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider>
-      <App />
-      <Toaster />
+let persistor=persistStore(store)
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <Toaster />
+      </PersistGate>
+
     </Provider>
-  </React.StrictMode>,
+  </StrictMode>,
 )
