7a4f50429441608422b74e326c65a0eca31c39a7
anna <annayunarai09@gmail.com>
2025-06-27 22:44:35 +0545

    Import createpost component

--- Frontend/src/components/LeftSidebar.jsx
+++ Frontend/src/components/LeftSidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Heart, Home, LogOut, MessageCircle, PlusSquare, Search, TrendingUp } from 'lucide-react'
-//import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import axios from 'axios';
 //import { useDispatch, useSelector } from 'react-redux'
 //import { setAuthUser } from '@/redux/authSlice'
@@ -9,64 +9,74 @@ import axios from 'axios';
 import { Button } from './ui/button'
 import { toast } from 'sonner'
 import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import store from '../redux/store';
+import { setAuthUser } from '../redux/authSlice';
+import CreatePost from './CreatePost';
+
+
 //import CreatePost from './CreatePost'
 
-const sidebarItems = [
-    { icon: <Home />, text: "Home" },
-    { icon: <Search />, text: "Search" },
-    { icon: <TrendingUp />, text: "Explore" },
-    { icon: <MessageCircle />, text: "Messages" },
-    { icon: <Heart />, text: "Notifications" },
-    { icon: <PlusSquare />, text: "Create" },
-    {
-        /*icon: (
-            <Avatar className="w-6 h-6">
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-        ),
-        text: "Profile"*/
-    },
-    { icon: <LogOut />, text: "Logout" },
-]
 
 const LeftSidebar = () => {
     const navigate = useNavigate();
-    /*const { user } = useSelector(store => store.auth);
-    const { likeNotification } = useSelector(store => store.realTimeNotification);
-    const dispatch = useDispatch();*/
-    //const [open, setOpen] = useState(false);
+    const { user } = useSelector(store => store.auth);
+    /*const { likeNotification } = useSelector(store => store.realTimeNotification);*/
+    const dispatch = useDispatch();
+    const [open, setOpen] = useState(false);
+    
 
     const logoutHandler = async () => {
-        
+
         try {
             const res = await axios.get('http://localhost:8000/api/v1/user/logout', { withCredentials: true });
             if (res.data.success) {
                 navigate("/login");
                 toast.success(res.data.message);
-                {/*dispatch(setAuthUser(null));
-                dispatch(setSelectedPost(null));
+                dispatch(setAuthUser(null));
+                {/*dispatch(setSelectedPost(null));
                 dispatch(setPosts([]));*/}
-                
+
             }
         } catch (error) {
             toast.error(error.response.data.message);
         }
     }
-
+  
     const sidebarHandler = (textType) => {
         if (textType == 'Logout') {
             logoutHandler();
-        } {/*else if (textType === "Create") {
+        } else if (textType === "Create") {
             setOpen(true);
-        } else if (textType === "Profile") {
+        } /*else if (textType === "Profile") {
             navigate(`/profile/${user?._id}`);
         } else if (textType === "Home") {
             navigate("/");
         } else if (textType === 'Messages') {
             navigate("/chat");
-        }*/}
+        }*/
     }
+
+
+    const sidebarItems = [
+        { icon: <Home />, text: "Home" },
+        { icon: <Search />, text: "Search" },
+        { icon: <TrendingUp />, text: "Explore" },
+        { icon: <MessageCircle />, text: "Messages" },
+        { icon: <Heart />, text: "Notifications" },
+        { icon: <PlusSquare />, text: "Create" },
+        {
+            icon: (
+                <Avatar className="w-6 h-6">
+                    <AvatarImage src={user?.profilePicture} alt="picture" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            ),
+            text: "Profile"
+        },
+        { icon: <LogOut />, text: "Logout" },
+    ]
+    
 
 
     return (
@@ -115,7 +125,7 @@ const LeftSidebar = () => {
                 </div>
             </div>
 
-            {/*<CreatePost open={open} setOpen={setOpen} />*/}
+            <CreatePost open={open} setOpen={setOpen} />
 
         </div>
     )
