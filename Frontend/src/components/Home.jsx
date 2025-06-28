a67bd8862c204e3d9fc00be4348dd5aac3c06b6b
anna <annayunarai09@gmail.com>
2025-06-28 17:43:52 +0545

    import react hooks

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Feed from "./Feed";
 import RightSidebar from "./RightSidebar";
+import useGetAllPosts from "../hooks/useGetAllPosts";
 const Home = () => {
+    useGetAllPosts();
     return (
         <div className='flex'>
             <div className='flex-grow'>
