ff601d2a84e475fcf1d9e22633bd91d33cdd7d1d
anna <annayunarai09@gmail.com>
2025-06-30 23:04:17 +0545

    Import useGetSuggestedUsers custom hook

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -3,8 +3,10 @@ import { Outlet } from "react-router-dom";
 import Feed from "./Feed.jsx";
 import RightSidebar from "./RightSidebar.jsx";
 import useGetAllPosts from "../hooks/useGetAllPosts";
+import useGetSuggestedUsers from "@/hooks/useGetSuggestedUsers.jsx";
 const Home = () => {
     useGetAllPosts();
+    useGetSuggestedUsers();
     return (
         <div className='flex'>
             <div className='flex-grow'>
