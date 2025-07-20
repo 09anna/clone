56453081c1de2b57b6885bbff588ca942f28923f
annayuna <annayunarai09@gmail.com>
2025-07-20 14:00:19 +0545

    Add custom hook to fetch and store user profile data

---
+++ Frontend/src/hooks/useGetUserProfile.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setUserProfile} from "@/redux/authSlice";
+
+const useGetUserProfile = (userId) => {
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+        const fetchUserProfile = async () => {
+            try {
+                const res = await axios.get(`http://localhost:8000/api/v1/user/${userId}/profile`, { withCredentials: true });
+                if (res.data.success) {
+                    //console.log(res.data);
+                    
+                    
+                    dispatch(setUserProfile(res.data.user));
+
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchUserProfile();
+    }, []);
+}
+export default useGetUserProfile;
