73b573efc9fecd91519b5c5c6e7bab89c21ab6ab
annayuna <annayunarai09@gmail.com>
2025-08-03 18:18:02 +0545

    Add Edit Profile section with user details

--- Frontend/src/components/EditProfile.jsx
+++ Frontend/src/components/EditProfile.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-const EditProfile=()=>{
+const EditProfile = () => {
+    const { user } = useSelector(store => store.auth);
     return (
-        <div>EditProfile</div>
+        <div className="flex max-w-2xl mx-auto pl-10">
+            <section className='flex flex-col gap-6 w-full my-8'>
+                <h1 className="font-bold text-xl">Edit profile</h1>
+                <div className='flex items-center justify-between bg-gray-100 rounded-xl p-4'>
+                    <div className='flex items-center gap-3'>
+                        <Avatar>
+                            <AvatarImage src={user?.profilePicture} alt="post_image" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                        <div >
+                            <h1 className='font-bold text-sm'>{user?.username}</h1>
+                            <span className='text-gray-600 '>{user?.bio || "Bio here..."}</span>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 }
 export default EditProfile;
