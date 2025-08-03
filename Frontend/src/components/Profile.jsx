d3bb2cca8cd74c0f3c54849b4ea3c9ad886f4e99
annayuna <annayunarai09@gmail.com>
2025-08-03 12:10:29 +0545

    Add some Css in the buttons and posts

--- Frontend/src/components/Profile.jsx
+++ Frontend/src/components/Profile.jsx
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import useGetUserProfile from "../hooks/useGetUserProfile.jsx";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
+import { AtSign, Heart, MessageCircle } from "lucide-react";
 const Profile = () => {
     const params = useParams();
     const userId = params.id;
     useGetUserProfile(userId);
+    const [activeTab, setActiveTab] = useState('posts');
 
     const { userProfile } = useSelector(store => store.auth);
     console.log(userProfile);
-    const isLoggedInUserProfile = false;
-    const isFollowing = true;
+    const isLoggedInUserProfile = true;
+    const isFollowing = false;
 
     //const isLoggedInUserProfile = user?._id === userProfile?._id;
+    const handleTabChanger = (tab) => {
+        setActiveTab(tab);
+    }
+    const displayedPost = activeTab === 'posts' ? userProfile?.posts : userProfile?.bookmarks;
 
     return (
         <div className='flex max-w-5xl justify-center mx-auto pl-10'>
@@ -34,14 +41,14 @@ const Profile = () => {
                                 {
                                     isLoggedInUserProfile ? (
                                         <>
-                                            <Button variant='secondary' className='hover:bg-gray-200 h-8'>Edit profile</Button>
+                                            <Link to="/account/edit"><Button variant='secondary' className='hover:bg-gray-200 h-8'>Edit profile</Button></Link>
                                             <Button variant='secondary' className='hover:bg-gray-200 h-8'>View archive</Button>
                                             <Button variant='secondary' className='hover:bg-gray-200 h-8'>Ad tools</Button>
                                         </>
                                     ) : (
                                         isFollowing ? (
                                             <>
-                                                <Button variant='secondary' className=' h-8'>Unfollow</Button>
+                                                <Button variant='secondary' className='h-8'>Unfollow</Button>
                                                 <Button variant='secondary' className='h-8'>Message</Button>
 
                                             </>
@@ -51,10 +58,66 @@ const Profile = () => {
 
                                     )
                                 }
+                            </div>
+                            <div className='flex items-center gap-4'>
+                                <p><span className='font-semibold'>{userProfile?.posts.length} </span>Posts</p>
+                                <p><span className='font-semibold'>{userProfile?.followers.length} </span>Followers</p>
+                                <p><span className='font-semibold'>{userProfile?.following.length} </span>Following</p>
+                            </div>
+                            <div className='flex flex-col gap-1'>
+                                <span className='font-semibold'>{userProfile?.bio || 'bio here...'}</span>
+                                {/*<Badge className='w-fit' variant='secondary'><AtSign/><span className='pl-1'>{userProfile?.username}</span></Badge>*/}
+                                <span>💻Hi! I am learning Web Development </span>
+                                <span>👩‍💻HTML! CSS! JS!</span>
+                                <span>🤯Beginner!!</span>
 
                             </div>
                         </div>
                     </section>
+                </div>
+                <div className='border-t border-t-gray-200'>
+                    <div className='flex items-center justify-center gap-10 text-sm'>
+                        <span className={`py-3 cursor-pointer ${activeTab === 'posts' ? 'font-bold' : ''}`} onClick={() => handleTabChanger('posts')}>Posts
+
+                        </span>
+                        <span className={`py-3 cursor-pointer ${activeTab === 'saved' ? 'font-bold' : ''}`} onClick={() => handleTabChanger('saved')}>Saved
+
+                        </span>
+                        <span className={`py-3 cursor-pointer ${activeTab === 'tags' ? 'font-bold' : ''}`} onClick={() => handleTabChanger('tags')}>Tag</span>
+                    </div>
+
+
+                    <div className='grid grid-cols-3 gap-6 max-w-5xl px-8'>
+                        {
+                            displayedPost?.map((post) => {
+                                return (
+                                    <div key={post?._id} className='relative group cursor-pointer '>
+                                        <img src={post.image} alt="Imagepost" className='rounded-sm my-2 w-full aspect-square object-cover ' />
+                                        <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+                                            <div className='flex items-center text-white space-x-4'>
+                                                <button className='flex items-center gap-2 hover:text-gray-300'>
+                                                    <Heart />
+                                                    <span>{post?.likes.length}</span>
+                                                </button>
+                                                <button className='flex items-center gap-2 hover:text-gray-300'>
+                                                    <MessageCircle />
+                                                    <span>{post?.comments.length}</span>
+                                                </button>
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+
+                                )
+                            })
+                        }
+
+                    </div>
+
+
+
+
                 </div>
             </div >
         </div >
